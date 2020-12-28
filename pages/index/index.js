9de3f8fabd96e110acabe6b1e6ba4e9e28170379
samsonhanku <hanku@samsonglobal.com>
2020-12-28 17:44:15 +0800

    clear some data

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,11 @@
 //index.js
 //获取应用实例
 const app = getApp()
-const wallet = require("../../utils/wallet")
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    motto: 'Hello World!',
+    userid: 123
   },
   //事件处理函数
   bindViewTap: function() {
@@ -17,45 +14,12 @@ Page({
     })
   },
   onShow : function(){
-    wallet.Forget("LV");
   },
   clickMe: function() {
     this.setData({ motto: "You Click Me" })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+
 })
